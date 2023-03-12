788d4dc7b883b4915221fa32df1ec7335e5ca06c
Arghya.Ghosh <Arghya.Ghosh@INLPF2SRVPV>
2023-03-12 16:07:22 +0530

    Mocking the injected service using createSpyObj method and spy on the methods - Angular

--- testing/angular-unit-testing/src/app/services/calculator/calculator.service.spec.ts
+++ testing/angular-unit-testing/src/app/services/calculator/calculator.service.spec.ts
@@ -1,15 +1,65 @@
 import { CalculatorService } from './calculator.service';
+import { LoggerService } from './Logger/logger.service';
 
 describe('CalculatorService', () => {
   xit('some test case pending', () => {});
+
+  /**
+   * With out using mocking and spying
+   * it is too much expensive operation.
+   */
+  /*
   it('should add two numbers', () => {
-    const calculatorService = new CalculatorService();
+    let loggerService = new LoggerService();
+    const calculatorService = new CalculatorService(loggerService);
     let result = calculatorService.add(2, 2);
     expect(result).toBe(4);
   });
   it('should subtract two number', () => {
-    const calculatorService = new CalculatorService();
+    let loggerService = new LoggerService();
+    const calculatorService = new CalculatorService(loggerService);
     let result = calculatorService.subtract(2, 3);
     expect(result).toBe(-1);
+  });
+  */
+
+  /**
+   * Using Spying
+   * It will spying the log method in the loggerService
+   */
+  /*
+  it('should add two numbers', () => {
+    let loggerService = new LoggerService();
+    spyOn(loggerService, 'log');
+    const calculatorService = new CalculatorService(loggerService);
+    let result = calculatorService.add(2, 2);
+    expect(result).toBe(4);
+    expect(loggerService.log).toHaveBeenCalledTimes(1);
+  });
+
+  it('should subtract two number', () => {
+    let loggerService = new LoggerService();
+    spyOn(loggerService, 'log');
+    const calculatorService = new CalculatorService(loggerService);
+    let result = calculatorService.subtract(2, 3);
+    expect(result).toBe(-1);
+    expect(loggerService.log).toHaveBeenCalledTimes(1);
+  });
+  */
+
+  it('should add two numbers', () => {
+    let loggerService = jasmine.createSpyObj('LoggerService', ['log']);
+    const calculatorService = new CalculatorService(loggerService);
+    let result = calculatorService.add(2, 2);
+    expect(result).toBe(4);
+    expect(loggerService.log).toHaveBeenCalledTimes(1);
+  });
+
+  it('should subtract two number', () => {
+    let loggerService = jasmine.createSpyObj('LoggerService', ['log']);
+    const calculatorService = new CalculatorService(loggerService);
+    let result = calculatorService.subtract(2, 3);
+    expect(result).toBe(-1);
+    expect(loggerService.log).toHaveBeenCalledTimes(1);
   });
 });
--- testing/angular-unit-testing/src/app/services/calculator/calculator.service.ts
+++ testing/angular-unit-testing/src/app/services/calculator/calculator.service.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@angular/core';
+import { LoggerService } from './Logger/logger.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CalculatorService {
+  constructor(private loggerService: LoggerService) {}
   add(n1: number, n2: number) {
     const result = n1 + n2;
+    this.loggerService.log('add method is called');
     return result;
   }
 
   subtract(n1: number, n2: number) {
+    this.loggerService.log('subtract method is called');
     const result = n1 - n2;
     return result;
   }
